eb657970ebb29e5334449dda7c21d2ea01358877
Anu <anubhaskar25@gmail.com>
2026-03-14 11:29:06 -0500

    Create Practice-converting-charactor-arrays.js
    tasks completed

---
+++ Practice-converting-charactor-arrays.js
@@ -0,0 +1,38 @@
+//Task1
+
+// Message 1
+let message1 = " !yako eb ll'uoy dna ,gniog peeK !sgnittes fo yteirav a ni slliks gnidoc esu osla nac uoY !ti teg ll'uoy ,tsisrep uoy fi tub ,tsrif ta drah mees yam gnidoC";
+let decoded1 = message1.split('').reverse().join('');
+console.log(decoded1);
+
+// Message 2
+let message2 = "!ecitcarp htiw retteb teg ll‘uoy ,emit ekaT .tsrif ta drah leef lliw gnitirw edoc tuB";
+let decoded2 = message2.split('').reverse().join('');
+console.log(decoded2);
+
+// Message 3
+let message3 = "!elpoep rehto morf tnereffid on era uoy ,elbuort evah uoy fI .lanoisseforp a ekil leef ot evah t'nod uoY";
+let decoded3 = message3.split('').reverse().join('');
+console.log(decoded3);
+
+// Message 4
+let message4 = ".rettam llits yeht ,smargorp llams etirw ylno nac uoy fI .tnemom tcefrep eht rof tiaw t'noD .yadot trats tsuJ";
+let decoded4 = message4.split('').reverse().join('');
+console.log(decoded4);
+
+//TASK2
+
+// Original inspirational messages
+let msg1 = "Keep learning and never give up";
+let msg2 = "Small progress every day leads to big success";
+let msg3 = "Believe in your ability to code";
+
+// Reverse the messages
+let reverse1 = msg1.split('').reverse().join('');
+let reverse2 = msg2.split('').reverse().join('');
+let reverse3 = msg3.split('').reverse().join('');
+
+// Log the reversed messages
+console.log(reverse1);
+console.log(reverse2);
+console.log(reverse3);
